11ad57ed2bbf5d51f06fbc4645fdd157e2fd7da8
dansiecrown <dansiecrown@gmail.com>
2022-11-05 02:20:55 +0100

    added more description text to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,9 @@ export default function Home() {
         <div className="home">
             <div className="container">
                 <h1> Welcome to My Altschool Project for the second semester</h1>
-                <p> This is a simple app that uses the Github API to display a list of repositories for a user. The app uses React Router to navigate between pages. It also uses Context to pass data between components.</p>
+                <p> This is a simple app that uses the Github API to display a list of repositories for a user. The app uses React Router to navigate between pages. It also uses Context to pass data between components. <em>Bonus:</em> Added a functionality that allows you to enter your github username and then fetch your github repos.</p>
+
+
 
                 <Link to="/list" className="btn">Get Started</Link>
 
